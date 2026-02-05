59750c910dab102807df0936b68d8e82544fa16d
Miguel Ramos <migue1990@gmail.com>
2026-02-04 22:20:05 -0600

    fix(checkstyle): fix CORS properties import spacing

--- backend/src/main/java/app/navilla/config/CorsProperties.java
+++ backend/src/main/java/app/navilla/config/CorsProperties.java
@@ -2,6 +2,7 @@ package app.navilla.config;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
